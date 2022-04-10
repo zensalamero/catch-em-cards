1c3b9ed35dbf012affc2a79789dd725e0637a9fc
Janzen Salamero <michaela.salamero@gmail.com>
2022-04-10 23:09:37 +0800

    moved card 1 animation on load

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 $(document).ready(function() {
-    // $('.fade-in-from-top').animate({'opacity': '1', 'margin-top': '0'}, 1000);
+    $('.fade-in-from-top').animate({'opacity': '1', 'margin-top': '0'}, 1000);
+    $(".fade-in-from-top div").each(function(j) {
+        if($(this)[0].className === "bolt") {
+            $(this).delay(400*j).slideDown('medium');
+        } else {
+            $(this).delay(400*j).fadeIn(300);
+        }
+    });    
     $(window).scroll( function() {
         var bottom_of_window = $(window).scrollTop() + $(window).height() + 700;
-
-        $('.fade-in-from-top').each( function(i) {
-            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-            if(bottom_of_window > bottom_of_element) {
-                $(this).animate({'opacity': '1', 'margin-top': '0'}, 1000);
-                $(".fade-in-from-top div").each(function(j) {
-                    if($(this)[0].className === "bolt") {
-                        $(this).delay(400*j).slideDown('medium');
-                    } else {
-                        $(this).delay(400*j).fadeIn(300);
-                    }
-                });                
-            }
-        })
 
         $('.fade-in-from-left').each( function(i) {
             var bottom_of_element = $(this).offset().top + $(this).outerHeight();
